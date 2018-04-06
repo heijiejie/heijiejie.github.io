b147b1a8952d0c7f1da880a87593c18c9cecf820
changli025 <changli025@163.com>
2018-04-06 18:13:54 +0800

    Site updated: 2018-04-06 18:13:52

--- 砖块重叠消除/js/checking.js
+++
@@ -1,3 +0,0 @@
-function search(twoD,oneD){
-	// Searches a two dimensional array to see if it contains a one dimensional array. indexOf doesn't work in this case
-	for(var i=0;i
--- 砖块重叠消除/js/comboTimer.js
+++
@@ -1,6 +0,0 @@
-function drawTimer() {
-	if(gameState==1){
-		var leftVertexes = [];
-		var rightVertexes = [];
-	if(MainHex.ct - MainHex.lastCombo < settings.comboTime){
-		for(var i=0;i
